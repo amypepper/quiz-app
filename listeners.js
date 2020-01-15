3a5df2fe949b76dcaf29ebef7bd0ac3ebf404ed3
Amy Pepper <amyandmicahpepper@gmail.com>
2020-01-15 01:03:22 -0800

    removed score.js

--- listeners.js
+++ listeners.js
@@ -72,4 +72,15 @@ function handleRetakeQuizClick() {
     console.log(`handleRetakeQuizClick ran`);
 };
 
+function evaluateAnswer(answer, itemIndex) {
+    // compares stored value from <label> element to the correct answer and calls either renderCorrect... or renderIncorrect...
+    if(answer === STORE[itemIndex].correctAnswer) {
+        renderCorrectAnswer();
+    };
+    if(answer !== STORE[itemIndex].correctAnswer) {
+        renderIncorrectAnswer();
+    }
+    console.log(`evaluateAnswer ran`);
+};
+
 handleStartClick();
